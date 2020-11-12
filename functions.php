a83ca39f5a02ab6b34c711f5995d3156a9f26e6f
Arth Modi <arthmodi36@gmail.com>
2020-11-12 14:52:19 +0530

    created blog page

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ function wpshp_theme_setup() {
 add_action( 'after_setup_theme', 'wpshp_theme_setup' ); 
 
 /******************************
-  * Custom Post Type
+  * Custom Portfolio Post Type
 ******************************/
 
 function wprtt_custom_portfolio_posttype(){
@@ -75,3 +75,54 @@ function wprtt_custom_portfolio_posttype(){
 }
 
 add_action( 'init', 'wprtt_custom_portfolio_posttype' );
+
+/******************************
+  * Custom Blog Post Type
+******************************/
+
+function wprtt_custom_blog_posttype(){
+
+    $labels = array(
+        'name'                    => 'Blogs',
+        'singular_name'           => 'Blog',
+        'add_new'                 => 'Add Blog',
+        'all_items'               => 'All Blogs',
+        'add_new_item'            => 'Add BLog',
+        'edit_item'               => 'Edit Blog',
+        'new_item'                => 'New Blog',
+        'view_item'               => 'View Blog',       
+        'search_item'             => 'Search Blog',
+        'not_found'               => 'No Blogs Found',
+        'not_found_in_trash'      => 'No Blogs Found In Trash',
+        'parent_item_colon'       => 'Parent Blog',
+    );
+
+    $args = array(
+        'labels'                    => $labels,
+        'public'                    => true,
+        'has_archive'               => true,
+        'publicly_queryable'        => true,
+        'query_var'                 => true,
+        'rewrite'                   => true,
+        'capability_type'           => 'post',
+        'hierarchical'              => false,
+        'supports'                  => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail',
+            'revisions',
+        ),
+        'taxonomies'                => array(
+            'category',
+            'post_tag',
+        ),
+        'menu_position'             => 5,
+        'exclude_from_search'       => false,
+    );
+
+    register_post_type( 'blog', $args );
+
+}
+
+add_action( 'init', 'wprtt_custom_blog_posttype' );
--- page_templates/page-blog.php
+++ page_templates/page-blog.php
@@ -42,6 +42,142 @@ get_header();
     </div>
 </div>
 
+<div class="blog_content_part">
+    <div class="whole_blog_section_container">
+        <div class="blog_wrapper">
+            <div class="blog_content_header">
+                <div class="inner_blog">
+                    <p class="lets_blog">LET'S BLOG</p>
+                </div>
+                <hr/>
+            </div>
+
+            
+                <?php
+
+                    $paged = get_query_var( 'paged' ) ;
+
+                    $args = array(
+                        'post_type'     => 'blog',
+                        'post_status'   => 'publish',
+                        'order'       => 'asc',
+                        'posts_per_page' => 5,
+                        'paged' => $paged, 
+                    );
+                    $posts = new WP_QUERY($args);
+                    
+                    if ( $posts -> have_posts() ) :
+                        
+                        while( $posts -> have_posts() ) :
+                            $posts -> the_post();
+                    ?>
+
+            <div class="blog_container">
+                <div class="blog_content">
+                    <div class="blog_content_top">
+                        <div class="date">
+                            <span><p class="date_p">22</p></span>
+                            <span><p class="month_p">DEC</p></span>
+                        </div>
+                        <div class="blog_content_title">
+                            <p><?php echo the_title(); ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="blog_content_bottom">
+                    <img class="blog_content_img" src="<?php echo the_post_thumbnail_url(); ?>"/>
+                    <div class="blog_content_right">
+                        <div class="blog_content_right_top">
+                            <p class="blog_auth">by <span class="blog_author_name">Robin Sen</span> on 21Dec 2012</p>
+                            <p class="blog_tot_comment">12 comments</p>
+                        </div>
+                        <hr class="blog_content_hr"/>
+                        <div class="blog_content_right_bottom">
+                            <p class="blog_content_right_bottom_p"><?php echo the_excerpt(); ?></p>
+                            <p class="blog_content_right_bottom_readmore">READ MORE</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
+        <?php
+            endwhile;
+            ?>
+            <div class="pagination_bar">
+            <?php
+                echo paginate_links( array(
+                    'total' => $posts->max_num_pages,
+                ) );
+            ?>
+            </div>
+        <?php
+                wp_reset_postdata();
+                else:
+            ?>
+                <div><h1>There is no blogs</h1></div>
+            <?php
+                endif;
+                    
+            ?>
+            
+        </div>
+        
+        <div class="blog_right">
+            <div class="blog_right_portfolio">
+                <p class="blog_right_portfolio_title">Portfolio</p>
+                <hr/>
+                <div class="blog_right_portfolio_img_container">
+                    <?php
+                        $args = array(
+                            'post_type'     => 'portfolio',
+                            'post_status'   => 'publish',
+                            'order'       => 'asc',
+                        );
+                        $posts = new WP_QUERY($args);
+                        if ( $posts -> have_posts() ) :
+                    ?>
+                    <div class="vertical">
+                        <?php
+                            for($i=0;$i<2;$i++){
+                        ?>
+                        <div class="horizontal">
+                            <?php
+                                for($j=0;$j<4;$j++){
+                                    $posts -> the_post();
+                                    if(has_post_thumbnail()):
+                            ?>
+                            <img class="blog_right_portfolio_img" src="<?php echo the_post_thumbnail_url(); ?>" />
+                            <?php
+                                endif;
+                            }
+                            ?>
+                        </div>
+                    <?php        
+                        }
+                    ?>
+                    </div>
+                <?php
+                    endif;
+                ?>
+                </div>
+            </div>
+
+            <div class="blog_right_popular_posts">
+                <p class="blog_right_popular_posts_title">Popular Posts</p>
+                <hr class="popular_post_hr"/>
+                <div class="blog_right_popular_posts_container">
+                    <img class="popular_posts_img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/image-1.png"/>
+                    <div class="popular_posts_content">
+                        <p class="popular_posts_content_upper">Archieve your grandest dreams!</p>
+                        <p class="popular_posts_content_lower">by <span class="blog_author_name">Robin Sen</span> on 21Dec 2012</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
 
 get_footer();
